456fa5f145281bf7bbf84478bab8756adac0363a
Marco Tello <marcotello5@outlook.com>
2024-08-21 13:57:25 -0700

    Completing Exercise 3, introducing a new pipe

--- src/app/exercise3/exercise3.component.ts
+++ src/app/exercise3/exercise3.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import {Observable} from 'rxjs';
+import {combineLatest, Observable, startWith} from 'rxjs';
 import {Country, State} from './types';
 import {CountryService} from './country.service';
+import {FormControl} from "@angular/forms";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-exercise3',
@@ -11,11 +13,19 @@ import {CountryService} from './country.service';
 export class Exercise3Component {
 
   countries$: Observable<Country[]> = this.service.getCountries();
+  countryControl = new FormControl('');
+  countryFilter$ = this.countryControl.valueChanges.pipe(startWith(''));
+  filteredCountries$ = combineLatest([this.countries$, this.countryFilter$])
+    .pipe(
+      map(([countries, countryFilter]) =>
+        countries.filter(c => c.description.toLowerCase().indexOf(countryFilter.toLowerCase())!== -1))
+    );
+
   states$: Observable<State[]>;
   country!: Country;
   state!: State;
 
-  constructor(private service: CountryService) { }
+  constructor(private service: CountryService) {}
 
   updateStates(country: Country) {
     this.country = country;
