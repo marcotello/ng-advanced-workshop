fda072aa7f3f0d225b305ce25a91522a24e8df15
Marco Tello <marcotello5@outlook.com>
2024-08-21 14:20:18 -0700

    Exercise 3 final solution after watching the video

--- src/app/exercise3/exercise3.component.ts
+++ src/app/exercise3/exercise3.component.ts
@@ -3,7 +3,7 @@ import {combineLatest, Observable, startWith} from 'rxjs';
 import {Country, State} from './types';
 import {CountryService} from './country.service';
 import {FormControl} from "@angular/forms";
-import {map} from "rxjs/operators";
+import {map, withLatestFrom} from "rxjs/operators";
 
 @Component({
   selector: 'app-exercise3',
@@ -12,6 +12,7 @@ import {map} from "rxjs/operators";
 })
 export class Exercise3Component {
 
+  /*
   countries$: Observable<Country[]> = this.service.getCountries();
   countryControl = new FormControl('');
   countryFilter$ = this.countryControl.valueChanges.pipe(startWith(''));
@@ -19,13 +20,22 @@ export class Exercise3Component {
     .pipe(
       map(([countries, countryFilter]) =>
         countries.filter(c => c.description.toLowerCase().indexOf(countryFilter.toLowerCase())!== -1))
-    );
+    );*/
+
+  countries$: Observable<Country[]>;
+  countryControl = new FormControl('');
 
   states$: Observable<State[]>;
   country!: Country;
   state!: State;
 
-  constructor(private service: CountryService) {}
+  constructor(private service: CountryService) {
+    this.countries$ = this.countryControl.valueChanges.pipe(
+      withLatestFrom(this.service.getCountries()),
+      map(([userInput, countries]) =>
+        countries.filter(c => c.description.toLowerCase().indexOf(userInput.toLowerCase())!== -1))
+    );
+  }
 
   updateStates(country: Country) {
     this.country = country;
