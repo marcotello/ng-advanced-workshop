7938f16b2433be235a46649e63c8efdfa5f095c0
Marco Tello <marcotello5@outlook.com>
2024-09-23 22:18:23 -0600

    Completing Exercise 6, adding custom directive that can be used in a button or link

---
+++ src/app/exercise6/directives/at-button.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AtButtonDirective } from './at-button.directive';
+
+describe('AtButtonDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AtButtonDirective();
+    expect(directive).toBeTruthy();
+  });
+});
